7cfd99fb7ec7eda8be2ecab872eb4831a61e7cae
NicoDerp <nicolai.gjerde.buch@gmail.com>
2022-12-19 19:23:52 +0000

    Older python support in main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from nai import *
 net = MLPNeuralNetwork([2, 3, 2], activation=ReLU)
 net.layers[0] = [0.1, 0.3]
 net.expectedOutput = [0.8, 0.5]
-net.learning_rate = 0.1
+net.learning_rate = 0.05
 
 print("Trying to get:", net.expectedOutput)
 
@@ -14,7 +14,11 @@ max_epochs = 10000
 epoch = 0
 
 #for epoch in range(100):
-while epoch < max_epochs and (err := net.globalError()) > 0.000001:
+while epoch < max_epochs:
+    err = net.globalError()
+    if err < 0.0000001:
+        break
+
     print(f"Epoch {epoch+1}/{max_epochs}")
 
     net.forwardPropagation()
